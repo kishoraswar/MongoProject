95662088edc62fbc9d6297492b277f8ccd462a96
kishoraswar <kishoraswar22@gmail.com>
2022-07-17 19:30:45 +0530

    Tes commit

---
+++ mongo_con.py
@@ -0,0 +1,15 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://mongo:Welcome2022@cluster0.itrfmco.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d = {
+    "name" : "kishor",
+    "email" : "kishoraswar22@gmail.com",
+    "surname" : "aswar"
+
+}
+
+db1 = client['mongotest']
+coll = db1['test1']
+coll.insert_one(d)
